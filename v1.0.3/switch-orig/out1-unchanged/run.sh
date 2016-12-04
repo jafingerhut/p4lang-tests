d20fff77b553f1152c42d483550b6576be998b79
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2016-12-04 15:28:50 -0800

    Check in updated bash scripts and output files using new --dot-format option for p4-graphs
    with .eps output files.  These have searchable text labels via Mac OS
    X's Preview.app

--- v1.0.3/switch-orig/out1-unchanged/run.sh
+++ v1.0.3/switch-orig/out1-unchanged/run.sh
@@ -2,6 +2,22 @@
 
 OPTS=""
 
+# To only generate eps format files for graphs, instead of png.
+#
+# At least with Mac OS X Preview.app, it can search for text in the
+# labels of the graph when the file is in eps format, but not for png,
+# and for some strange reason not even for pdf.
+#
+# On Ubuntu 14.04 LTS Document View app, I could not successfully
+# search for text labels with any of these formats.  Edit->Find was
+# not even a selectable menu choice with eps files.  It was with pdf,
+# but no matches were found, like with Preview.app.
+#
+# It is a little bit of a down side that Github does not render eps
+# files graphically the way it does for pdf and png, but I prefer text
+# searchability on my Mac for now.
+OPTS="$OPTS --dot-format eps"
+
 # To print details of how the minimum stage of each table access is
 # determined.
 OPTS="$OPTS --deps-debug-count-min-stages"
